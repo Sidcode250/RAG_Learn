dee105b0dbe12adb8d8ffe20b50e36a12d5b80c3
Siddharth <sidcode25@gmail.com>
2026-09-02 16:29:58 +0530

    3

---
+++ app/config.py
@@ -0,0 +1,17 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Settings:
+    GROK_API_KEY = os.getenv("GROK_API_KEY")
+    GROK_MODEL = "llama-3.3-70b-versatile"
+    GROK_FALLBACK_API_KEY = os.getenv("GROK_FALLBACK_API_KEY")
+    
+    QDRANT_API_KEY = os.getenv("QDRANT_API_KEY")
+    QDRANT_URL = os.getenv("QDRANT_CLUSTER_ENDPOINT")
+    QDRANT_COLLECTION = "Enterprise_RAG_Learn"
+    GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+    #GEMINI_FALLBACK_API_KEY = os.getenv("GEMINI_FALLBACK_API_KEY")
+
+settings = Settings()
